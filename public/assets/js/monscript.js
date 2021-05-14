9f8ca1c56048ddb481a429a93a36654182cf6ea7
faby-dev <fabyandriampany@gmail.com>
2021-05-14 11:23:08 +0200

    Requete Ajax mettre en place

--- public/assets/js/monscript.js
+++ public/assets/js/monscript.js
@@ -7,11 +7,30 @@ $(document).ready(function(){
     });
     $('#ajoutEtudiants').on('submit', function(e){
         e.preventDefault();
-       let nom =  $(this).find('input[name="nom"]').val();
-       let prenom = $(this).find('input[name="prenom"]').val();
-       let sexe = $(this).find('option:selected');
-       console.log(nom);
-       console.log(prenom);
-       console.log(sexe);
-    })
+        let nom =  $(this).find('input[name="nom"]').val();
+        let prenom = $(this).find('input[name="prenom"]').val();
+        let sexe = $('#sexe').val();    
+        let date = $(this).find('input[name="datedenaissance"]').val();    
+
+        let jsonEnvoi = JSON.stringify({ "nom": nom, "prenom": prenom, "sexe": sexe, "dateDeNaissance": date })         
+      
+        
+       $.ajax({
+        url : 'http://localhost:3000/etudiant/create',
+        method: "POST",
+        data: jsonEnvoi,
+        dataType: "json",
+        success: function(response){
+            console.log(response);
+            $("#exampleModal").modal('hide')
+        },     
+        error: function(response) {
+            console.log(response)
+        }       
+        
+        })  
+         
+        
+    });
+    
 })
